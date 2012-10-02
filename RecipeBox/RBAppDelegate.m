b20e17a4c901f18b4d73d1e1a1eb4e3a5914c67a
Alexander Kislitsyn <alexander.kislitsyn@sigmaukraine.com>
2012-10-02 12:42:26 +0300

    Conflicting change

--- RecipeBox/RBAppDelegate.m
+++ RecipeBox/RBAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    // NOTE: This is a conflicting change
     return YES;
 }
 							
